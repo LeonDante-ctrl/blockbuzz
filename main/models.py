b742b5a76137d3b15080554abdf81827d142b3d3
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-23 11:12:50 +0300

    figured out why i couldnt call upon my news class as i set up a relationship in my comment class

--- main/models.py
+++ main/models.py
@@ -1,26 +1,36 @@
 from django.db import models
+
 class Category(models.Model):
-    title = models.CharField(max_length=200)
-    category_image=models.ImageField(upload_to='images/')
-    
+    title=models.CharField(max_length=200)
+    category_image=models.ImageField(upload_to='imgs/')
+
     class Meta:
         verbose_name_plural='Categories'
-        
+
     def __str__(self):
         return self.title
 
+# News Model
 class News(models.Model):
-    #relationship set as my neighbourhood foreign key below
-    category=models.ForeignKey(Category,on_delete=models.CASCADE) #all news set here will delete on command
-    title=models.CharField(max_length=200)
-    image=models.ImageField(upload_to='images/')
-    detail=models.TextField() 
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    title=models.CharField(max_length=300)
+    image=models.ImageField(upload_to='imgs/')
+    detail=models.TextField()
     add_time=models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         verbose_name_plural='News'
-        
+
     def __str__(self):
         return self.title
 
-# Create your models here.
+# Comments
+class Comment(models.Model):
+    news=models.ForeignKey(News,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=200)
+    comment=models.TextField()
+    status=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comment
