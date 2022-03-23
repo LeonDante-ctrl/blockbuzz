20563f1e4547cbeb718d52d55ffd23ce4f2f1b7c
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-23 11:26:05 +0300

    added a section for people to comment under said news post

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
-from .models import Category,News
+from .models import Category,News,Comment
 
 admin.site.register(Category)
 
 class AdminNews(admin.ModelAdmin):
-    list_display=('title','category', 'add_time')
+    list_display=('title','category','add_time')
+
 admin.site.register(News,AdminNews)
-# Register your models here.
+
+class AdminComment(admin.ModelAdmin):
+    list_display=('news','email','comment','status')
+admin.site.register(Comment,AdminComment)
