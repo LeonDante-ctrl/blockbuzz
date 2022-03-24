0737f3497e80cb050a6cf45157bbae7b87401417
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-24 09:54:02 +0300

    added the linking and extra url for all news

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.home,name='home')
+    path('', views.home,name='home'),
+    path('all_news', views.all_news,name='all_news'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from .models import News
+from .models import Category, News
 
 def home(request):
     first_news=News.objects.first()
     three_news=News.objects.all()[1:3]
+    three_categories=Category.objects.all()[0:3]
     
     return render(request,'home.html', {
         'first_news':first_news,
-        'three_news':three_news
+        'three_news':three_news,
+        'three_categories':three_categories,
     })
+    
+def all_news(request):
+    all_news=News.objects.all()
+    return render(request,'all-news.html', {
+        'all_news':all_news
+    })    
 # Create your views here.
